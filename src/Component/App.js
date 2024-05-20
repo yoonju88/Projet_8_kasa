1c1d92a95d592f32914474ed031e9c7bf8b66a20
yoonju88 <yoonju.t@gmail.com>
2024-05-20 15:38:14 +0200

    reset for projet Kasa

---
+++ src/Component/App.js
@@ -0,0 +1,14 @@
+
+import '../Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
