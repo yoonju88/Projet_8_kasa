727c84d306906c255f3980126fb58d9f4cb18d40
yoonju88 <yoonju.t@gmail.com>
2024-05-23 16:32:12 +0200

    Cleaning up code

--- src/Component/Accueil.js
+++ src/Component/Accueil.js
@@ -3,8 +3,17 @@ import '../Styles/index.css'
 import {GalleriesList} from "../Data/GalleriesList";
 import BannerImage1 from '../Assets/home_banner.jpg'
 import GalleryItem from './GalleryItem'
+import Logement from './Logement';
 
 function Accueil(){
+    const galleriesList = GalleriesList.map(gallery => (
+            <GalleryItem
+                key={gallery.id}
+                galleryId={gallery.id}
+                title={gallery.title}
+                cover={gallery.cover}
+            />
+    )) 
     return (
         <div className='accueil'>
             <div className='banner'>
@@ -12,19 +21,7 @@ function Accueil(){
                 <h2 className='banner_title'>Chez vous, partout et ailleurs</h2>
             </div>
             <div className='gallery'>
-                {GalleriesList.map(gallery => (
-                        <GalleryItem key={gallery.id}
-                        title={gallery.title}
-                        cover={gallery.cover}
-                        description={gallery.description} 
-                        pictures={gallery.pictures}
-                        host={gallery.host}
-                        rating={gallery.rating}
-                        location={gallery.location}
-                        equipements={gallery.equipments}
-                        tags={gallery.tags}                        
-                        />   
-                ))}
+                {galleriesList}
             </div>
         </div>
     )
