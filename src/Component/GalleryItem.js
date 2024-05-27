dc754a216923a59afb8a98825e30f7c074c15bf2
yoonju88 <yoonju.t@gmail.com>
2024-05-27 15:08:01 +0200

    Clean up code

--- src/Component/GalleryItem.js
+++ src/Component/GalleryItem.js
@@ -1,18 +1,20 @@
 import React from "react"
 import '../Styles/index.css'
 import { Link } from 'react-router-dom'
-import Logement from './Logement'
+
 
 
 function GalleryItem({galleryId, title, cover}) {
-  
+
+
     return(
         <div className="gallery-item" > 
             <Link to={`/Logement/${galleryId}`}>
-                <img src={cover} alt={title} className="gallery_item_image" />
+                <img src={cover} alt={`${title}`} className="gallery_item_image" />
                 <div className="gallery_image-overlay"></div>
                 <h3 className="gallery_item_title">{title}</h3>
             </Link>
+
         </div>
     )
 }
