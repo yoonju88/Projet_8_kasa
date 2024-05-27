fb361d1875484b280d779226afcc64d02101bef7
yoonju88 <yoonju.t@gmail.com>
2024-05-27 15:08:29 +0200

    Add Errorpage.js

---
+++ src/Component/ErrorPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link,} from 'react-router-dom'
+
+
+function ErrorPage (){
+    return (
+        <div className='errorPage'>
+            <h1 className='errorPage_title'>404</h1>
+            <p className='errorPage_message'>Oups! La page que vous demnadez n'existe pas.</p>
+            <p className='errorPage_link'><Link to="/">Retourner sur la page d'accueil</Link></p>
+        </div>
+    )
+
+}
+
+export default ErrorPage
