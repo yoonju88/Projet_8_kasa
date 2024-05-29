b0ed412e68adbe05959939cfe0768992e76a45f2
yoonju88 <yoonju.t@gmail.com>
2024-05-29 16:52:36 +0200

    Replaced file+ component name to 'APropos'

---
+++ src/Component/APropos.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import AproposBannerImage from '../Assets/apropos_banner.jpg'
+import '../Styles/index.css'
+import { contentList } from '../Data/contentList'
+import ContentItem from './AproposItem'
+import Banner from './Banner'
+
+function APropos(){
+    const bannerImage = AproposBannerImage
+    const bannerId = 'Apropos banner image'
+    const bannerTitle =''
+    
+    return (
+        <section className='apropos'>
+            <Banner bannerImage={bannerImage} bannerTitle={bannerTitle} id={bannerId}/>
+            <div className='apropos_list'>
+                {contentList.map(content =>(
+                    <ContentItem key={content.id}
+                    title={content.title}
+                    description={content.content} />
+                ))}              
+            </div>     
+        </section>
+    )        
+}
+
+export default APropos
--- src/Component/App.js
+++ src/Component/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import '../Styles/index.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom' 
 import Header from './Header'
-import A_Propos from './A_Propos'
+import APropos from './APropos'
 import Accueil from './Accueil'
 import Footer from './Footer'
 import Logement from './Logement'
@@ -15,13 +15,12 @@ function App() {
         <Header />
           <Routes>
             <Route path="/" element={<Accueil />} />
-            <Route path="/A_Propos" element={<A_Propos />}/>
+            <Route path="/A_Propos" element={<APropos />}/>
             <Route path="/Logement/:galleryId" element={<Logement />}/>
-            <Route path="*" element={<ErrorPage />} />
+            <Route path="*" element={<ErrorPage />} /> 
           </Routes>
           <Footer />
     </div>
   )
 }
-
 export default App;
